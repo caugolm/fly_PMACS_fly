235d834bc2082980cd11ff239c61ab92f720bf3b
hamsiradhakrishnan <hamsanandiniradhakrishnan@gmail.com>
2025-10-02 11:39:13 -0400

    Edited dcm2bids_helper to just convert to nifti and NOT delete.

--- dcm2bids_helper.sh
+++ dcm2bids_helper.sh
@@ -1,52 +1,43 @@
 #!/bin/bash
 
-
-if [[ $# -lt 7 ]] ; then
-    echo "USAGE: ./dcm2bids_helper.sh <subID> <sesID> <dicom_base> <bids_base> "
+eval "$(conda shell.bash hook)"
+conda activate /project/ftdc_volumetric/hup6_xa60/envs/dcm2bids
+# module load dcm2niix/
+if [[ $# -lt 3 ]] ; then
+    echo "USAGE: ./dcm2bids_helper.sh <subj> <sess> <config>"
+    echo "  runs dcm2niix and outputs in bids_base/tmp_dcm2bids, where bids_base is defined in config (as is input dicom_base and conda_env_path) "
     exit 1
 fi
 
+
 subj=$1
 sess=$2
-dicom_base=$3
-bids_base=$4
+config=$3
 
+source $config
 
-dicom_dir=${dicom_base}/${subj}/${sess}
-uncompressed_dicom_dir=${dicom_base}/${subj}/${sess}/uncompressed/
 eval "$(conda shell.bash hook)"
+conda activate $dcm2bids_conda_path
 
 echo "Running dcm2niix for subject: $subj and session: $sess"
-
-
-nifti_dir=${bids_base}/tmp_dcm2bids/sub-${subj}_ses-${sess}
-
-rm -rf ${nifti_dir} #avoiding conflicts with previous runs
-mkdir -p ${nifti_dir}
-#  
-if [ -e "${dicom_dir}/dicoms.tar" ] ; then
-    echo "Unzipping dicoms.tar"
-    tar -xvf ${dicom_dir}/dicoms.tar -C ${dicom_dir}
-else
-    if [[ ! -d ${uncompressed_dicom_dir} ]] ; then
-        mkdir -p ${uncompressed_dicom_dir}
-    fi
-    mv ${dicom_dir}/* ${uncompressed_dicom_dir}
-fi
-
-
-for file in ${uncompressed_dicom_dir}/*.zip; do
-    zipname=$(echo $file | rev | cut -d '/' -f1 | rev | sed 's/.zip/_unzipped/')
-    unzip -o ${file} -d ${uncompressed_dicom_dir}/${zipname}/
-done
+input_dir=/project/ftdc_volumetric/hup6_xa60/dicoms/${subj}/${sess}
+output_dir=/project/ftdc_volumetric/hup6_xa60/bids/tmp_dcm2bids/sub-${subj}_ses-${sess}
+input_dir=${dicom_base}/${subj}/${sess}
+output_dir=${bids_base}/tmp_dcm2bids/sub-${subj}_ses-${sess}
+rm -rf ${output_dir}
+mkdir -p ${output_dir}
 
 
 # Run dcm2niix:
-dcm2niix -b y -ba n -z y -f %3s_%f_%p_%t -o ${nifti_dir} ${uncompressed_dicom_dir}
+dcm2niix -b y -ba n -z y -f %3s_%f_%p_%t -o ${output_dir} ${input_dir}
 
-# subjlist="x"
+
+# # Usage:
+# subjlist=/project/ftdc_volumetric/hup6_xa60/lists/EG_20250625_NACCSC.csv
+# mkdir -p /project/ftdc_volumetric/hup6_xa60/logs/dcm2bids_helper/
 # for i in `cat $subjlist` ; do
 # 	subj=`echo "$i" | cut -f1 -d ','`
 #     sess=`echo "$i" | cut -f2 -d ','`
-#     /project/ftdc_volumetric/hup6_xa60/scripts/fly_PMACS_fly/dcm2bids_helper.sh $subj $sess $dicom_base $bids_base
-# done
+#     echo "Submitting job for subject: $subj and session: $sess"
+#     bsub -B -N -J ${subj}_${sess} -q bsc_short -o /project/ftdc_volumetric/hup6_xa60/logs/dcm2bids_helper/sub-${subj}_ses-${sess}.txt -n 1 /project/ftdc_volumetric/hup6_xa60/scripts/dcm2bids_helper.sh $subj $sess
+# done    
